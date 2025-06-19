45545962a927dc75938132dfe86461a2766d3d2f
OnealWrng <96973639+YuChainWeng@users.noreply.github.com>
2025-06-19 10:37:39 +0800

    Fix CFARRP SimObject to derive from LRURP

--- gem5/src/mem/cache/replacement_policies/CFARRP.py
+++ gem5/src/mem/cache/replacement_policies/CFARRP.py
@@ -1,6 +1,7 @@
 from m5.params import *
-from ReplacementPolicies import BaseReplacementPolicy
+from ReplacementPolicies import LRURP
 
-class CFARRP(BaseReplacementPolicy):
+class CFARRP(LRURP):
     type = 'CFARRP'
+    cxx_class = 'CFARRP'
     cxx_header = 'mem/cache/replacement_policies/cfar_rp.hh'
